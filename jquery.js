306e64cc1e2769c97468d598446b8ddd97686359
upexcel <rahul.excel2011@gmail.com>
2018-06-30 17:28:00 +0530

    updated using thead and tbody

--- jquery.js
+++ jquery.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-	$("table").append("<tr><th>First Name</th><th>Last Name</th><th>DOB</th><tr>");
   	$.ajax({
   		type:'GET',
         url: "data.json",
         datatype: 'json',
   	  	success: function(data) {
+  	  		var output = "";
   	  		for(i in data){
-  	 			$("table").append("<tr><td>"+data[i].firstname+"</td><td>"+data[i].lastname+"</td><td>"+data[i].dob+"</td></tr>");
+  	 			output += "<tr><td>" + data[i].firstname + "</td><td>" + data[i].lastname + "</td><td>" + data[i].dob + "</td></tr>";
   	  		}
+  			$("tbody").html(output);
   	 	}
   	});
-  });
+});
